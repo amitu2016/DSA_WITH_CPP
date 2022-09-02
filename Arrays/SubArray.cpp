c17014eb961ca353a0b09ca6507cc9f7e35525b7
Amit Upadhyay <amitu@cdac.in>
2022-09-02 23:24:33 +0530

    Subarray

---
+++ Arrays/SubArray.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+
+using namespace std;
+
+void printSubArray(int arr[], int n){
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i; j < n; j++)
+        {
+            for (int k = i; k <= j; k++)
+            {
+                cout << arr[k] << ",";
+            }
+            cout << endl;
+        }
+        
+    }
+    
+}
+
+int main(int argc, char const *argv[])
+{
+    int arr[] = {10,20,30,40,50,60,70};
+
+	int n = sizeof(arr)/sizeof(int);
+
+    printSubArray(arr,n);
+    return 0;
+}
